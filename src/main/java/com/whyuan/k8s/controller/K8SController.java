a379d63f3ae9add11b244b9bcc2a5e0b972fdd20
whyuan <455206833@qq.com>
2019-12-14 19:49:24 +0800

    init

---
+++ src/main/java/com/whyuan/k8s/controller/K8SController.java
@@ -0,0 +1,19 @@
+package com.whyuan.k8s.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈http://localhost:8080/k8s〉
+ *
+ * @author whyuan
+ * @create 2019/12/8
+ * @since 1.0.0
+ */
+@RestController
+public class K8SController {
+    @RequestMapping("/k8s")
+    public String k8s() {
+        return "hello K8s,i am whyuan!";
+    }
+}
